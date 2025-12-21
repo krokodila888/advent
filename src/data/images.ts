a86334705f061d5b6cf9154211a4a348239402f9
krokodila888 <krokodila888@yandex.ru>
2025-12-22 00:15:13 +0300

    FEAT: more data

--- src/data/images.ts
+++ src/data/images.ts
@@ -3,11 +3,19 @@ import katrina from '../images/katrina.jpg';
 import pink from '../images/pink.jpg';
 import rese from '../images/rese.jpg';
 import Ofrenda from '../images/Ofrenda.jpg';
+import kolumbia from '../images/kolumbia.webp';
+import kolumb2 from '../images/kolumb2.webp';
+import melhior from '../images/melhior.webp';
+import shir from '../images/shir.jpg';
 
 export const images = {
   muerte,
   katrina,
   pink,
   rese,
-  Ofrenda
+  Ofrenda,
+  kolumb2,
+  kolumbia,
+  melhior,
+  shir,
 };
