11b81842cf0dcc7138536fb60d84bd50bd921a4f
krokodila888 <krokodila888@yandex.ru>
2025-12-21 15:10:38 +0300

    FEAT: more data

--- src/data/images.ts
+++ src/data/images.ts
@@ -2,10 +2,12 @@ import muerte from '../images/muerte.jpg';
 import katrina from '../images/katrina.jpg';
 import pink from '../images/pink.jpg';
 import rese from '../images/rese.jpg';
+import Ofrenda from '../images/Ofrenda.jpg';
 
 export const images = {
   muerte,
   katrina,
   pink,
-  rese
+  rese,
+  Ofrenda
 };
